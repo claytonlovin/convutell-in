831bf43099cd8baea628588f9f9aa25ab8ab6a58
convutell <claytton.jesus@gmail.com>
2023-11-25 13:34:34 -0300

    finishing test step 2 of 5

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from views.logs import logs_bp
 from views.time import times_bp
 from views.conection import connection_bp
 from views.script import script_bp
+from views.pyscript import pyscript_bp
 #from connections import connections_bp
 from decouple import config
 
@@ -13,7 +14,7 @@ app.register_blueprint(logs_bp, url_prefix='/logs')
 app.register_blueprint(times_bp, url_prefix='/times')
 app.register_blueprint(connection_bp, url_prefix='/connection')
 app.register_blueprint(script_bp, url_prefix='/script')
-
+app.register_blueprint(pyscript_bp, url_prefix='/pyscript')
 
 if __name__ == '__main__':
     app.run(debug=True)
